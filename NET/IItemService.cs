166cff3d3c7ee566aef8b2dc0f5bc08fcec8ee06
Alex <125499491+agolomb@users.noreply.github.com>
2023-06-21 11:55:35 -0700

    Update and rename ICourseService.cs to IItemService.cs

---
+++ NET/IItemService.cs
@@ -0,0 +1,18 @@
+﻿using MoneFi.Models;
+using MoneFi.Models.Domain;
+using MoneFi.Models.Requests;
+using System.Collections.Generic;
+
+namespace MoneFi.Services.Interfaces
+{
+    public interface IItemService
+    {
+        Item Get(int id);
+        List<ItemSubject> GetSubjects();
+        Paged<Item> GetCreatedByPaginated(int id, int page, int pageSize);
+        int Add(ItemAddRequest model, int userId);
+        Paged<Item> SearchPagination(int pageIndex, int pageSize, string query, int? lookUpTypeId);
+        Paged<Item> GetPaginated(int pageIndex, int pageSize);
+        void Update(ItemUpdateRequest model, int userId);
+    }
+}
