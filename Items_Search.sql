91fcd36abb7babc962f456ae76912fcf473bed88
Alex <125499491+agolomb@users.noreply.github.com>
2023-06-21 10:45:44 -0700

    Update and rename Courses_Search.sql to Items_Search.sql

---
+++ Items_Search.sql
@@ -0,0 +1,61 @@
+ALTER PROC [dbo].[Items_Search]
+@PageIndex INT
+,@PageSize INT
+,@Query NVARCHAR(50) = null
+
+AS
+
+/*
+DECLARE 
+	@PageIndex INT = 0
+	,@PageSize INT = 6
+	,@Query nvarchar(50) = '6'
+	
+	EXECUTE [dbo].[Items_Search]
+		@PageIndex 
+		,@PageSize 
+		,@Query
+
+select * from dbo.items
+*/
+
+BEGIN
+
+	SELECT    c.Id
+		  ,c.Title
+		  ,c.Subject
+		  ,c.Description
+		  ,u.FirstName AS InstructorFirstName
+		  ,u.Mi AS InstructorMiddleInitial
+		  ,u.LastName AS InstructorLastName
+		  ,c.Duration
+		  ,LookUpType = (
+				SELECT DISTINCT l.Id, l.Name
+				FROM [dbo].[LookUpTypesOne] AS l join [dbo].[Items] AS co
+				ON l.Id = co.LookUpTypeId
+				WHERE l.Id = lt.Id
+				FOR JSON AUTO)
+		  ,c.CoverImageUrl
+		  ,s.[Name] AS StatusName
+		  ,c.DateCreated
+		  ,c.DateModified
+		  ,u.FirstName AS CreatedByFirstName
+	      ,u.Mi AS CreatedByMiddleInitial
+	      ,u.LastName AS CreatedByLastName
+	      ,u.FirstName AS ModifiedByFirstName
+	      ,u.Mi AS ModifiedByMiddleInitial
+	      ,u.LastName AS ModifiedByLastName,
+  
+	TotalCount = COUNT(1) OVER()
+  
+  
+	  FROM [dbo].[Items] AS c join [dbo].[Users] AS u ON c.InstructorId = u.Id
+			join [dbo].[LookUpTypesOne] AS lt ON c.LookUpTypesOneId = lt.Id
+			join [dbo].[LookUpTypesTwo] AS s ON c.LookUpTypesTwoId = s.Id
+
+	  WHERE (@Query IS NULL OR [Title] LIKE '%' + @Query + '%' OR lt.[Name] LIKE '%' + @Query + '%')
+
+	  ORDER BY c.Id ASC
+	  OFFSET (@PageIndex) * @PageSize ROWS
+	  FETCH NEXT @PageSize ROWS ONLY
+END
